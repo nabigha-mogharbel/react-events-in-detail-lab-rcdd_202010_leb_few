9086e7054bba74f6b735bdc4a8c6556a406449e5
Nabigha Mogharbel <nabiga.ba.mo1998@gmail.com>
2021-01-27 23:12:58 +0000

    Automatically backed up by Learn

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,15 @@
 // Code CoordinatesButton Component Here
+import React from "react"
+
+function onReceiveCoordinates(onReceiveCoordinates){
+  let coor=[]
+  function coordinate(){
+    let x=event.clientX
+    let y=event.clientY
+    coor.push(x,y)
+  }
+  return(
+    <button onClick="coordinate()"></button>
+    
+    )
+}
